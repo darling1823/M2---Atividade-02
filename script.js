c0bb9f191175e32aaf624d42715ebbe92167e232
darling1823 <gabrielalcantara1823@gmail.com>
2022-06-11 10:14:46 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,36 @@
+let age = 18
+let human = true
+if(age >= 18){
+    console.log('você é maior de idade')
+}else{
+    console.log('você não deveria estar aqui')
+}
+if(age >= 18 && human == true){
+    console.log('acesso permitido')
+}else{
+    console.log('acesso negado')
+}
+
+let january = true
+let december = true
+if(january == true || december == true){
+    console.log('happy birthday')
+}else{
+    console.log('você não é sagitário nem capricórnio')
+}
+
+let firstLetter = 'R'
+if(firstLetter === 'R'){
+    console.log('raridade')
+}else{
+    console.log('normal')
+}
+
+let numberOfLetters = 6
+let firstLetter2 = 'E'
+if(numberOfLetters == 6 || firstLetter2 === 'E'){
+    console.log('welcome')
+}else{
+    console.log('youre not welcome')
+}
+
